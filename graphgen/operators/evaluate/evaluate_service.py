a2572468fb56314d65d2b5b38e7185244b83b8fd
chenzihong <522023320011@smail.nju.edu.cn>
2025-12-24 23:44:00 +0800

    wip: add annotations

--- graphgen/operators/evaluate/evaluate_service.py
+++ graphgen/operators/evaluate/evaluate_service.py
@@ -10,13 +10,24 @@ class EvaluateService(BaseOperator):
     2. QA Quality Evaluation
     """
 
-    def __init__(self, working_dir: str = "cache"):
+    def __init__(self, working_dir: str = "cache", metrics: list[str] = None):
+        # optional 传入 graph
         super().__init__(working_dir=working_dir, op_name="evaluate_service")
         self.llm_client: BaseLLMWrapper = init_llm("synthesizer")
+        self.metrics = metrics or []
+
+        self.evaluators = {
+            "xxx": "xxxEvaluator"
+        }
+
+        self.graph_storage = init_storage(
+            xx, xx, xx
+        )
 
     def process(self, batch: pd.DataFrame) -> pd.DataFrame:
         items = batch.to_dict(orient="records")
         return pd.DataFrame(self.evaluate(items))
 
     def evaluate(self, items: list[dict]) -> list[dict]:
+        # 用evaluators 评估 items
         pass
